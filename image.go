951f0dcf6a51c52540f14d05a4c5700cf3a26391
lucas-science <lucaslhomme01@gmail.com>
2025-01-12 13:35:42 +0100

    feat : add go routine to create RGB matrix

--- image.go
+++ image.go
@@ -3,41 +3,40 @@ package main
 import (
 	"fmt"
 	"image"
-	"image/color"
 	_ "image/jpeg"
 	"math"
 	"os"
+	"sync"
 )
 
-struct RGB {
+type RGB struct {
 	R, G, B int
-} 
+}
 
 func getImageData(path string) (image.Image, string, error) {
 	fmt.Println("Récupération des données de ", path)
 	imgFile, err := os.Open(path)
-
 	if err != nil {
-		fmt.Println("image not found")
+		fmt.Println("Image introuvable")
 		return nil, "", err
 	}
 	defer imgFile.Close()
 
 	imageData, imageType, err := image.Decode(imgFile)
 	if err != nil {
-		fmt.Println("Failed to decode the image:", err)
+		fmt.Println("Impossible de décoder l'image:", err)
 		return nil, "", err
 	}
-	return imageData, imageType, err
+	return imageData, imageType, nil
 }
 
-func distanceEuclidienneRGB(pixel1 Array, pixel2 Array) float64 {
-	r1, g1, b1 := pixel1[0], pixel1[1], pixel1[2]
-	r2, g2, b2 := pixel2[0], pixel2[1], pixel2[2]
-	return math.Sqrt(math.Pow(r1-r2, 2) + math.Pow(g1-g2, 2) + math.Pow(b1-b2, 2))
+func distanceEuclidienneRGB(pixel1 RGB, pixel2 RGB) float64 {
+	r1, g1, b1 := pixel1.R, pixel1.G, pixel1.B
+	r2, g2, b2 := pixel2.R, pixel2.G, pixel2.B
+	return math.Sqrt(math.Pow(float64(r1-r2), 2) + math.Pow(float64(g1-g2), 2) + math.Pow(float64(b1-b2), 2))
 }
 
-func rowProcessDistance(row1, row2 []Array) float64 {
+func rowProcessDistance(row1, row2 []RGB) float64 {
 	var distance float64
 	for i := 0; i < len(row1); i++ {
 		distance += distanceEuclidienneRGB(row1[i], row2[i])
@@ -45,11 +44,12 @@ func rowProcessDistance(row1, row2 []Array) float64 {
 	return distance
 }
 
-func addRGBRow(imageData image.Image, y int, matrice *[][]RGB) []Array {
-	vidthmax := imageData.Bounds().Max.X
-	row := make([]color.Color, widthmax)
+func createRGBRow(imageData image.Image, y int) []RGB {
+	widthmax := imageData.Bounds().Max.X
+	row := make([]RGB, widthmax)
 	for x := 0; x < widthmax; x++ {
-		row[x] = imageData.At(x, y)
+		r, g, b, _ := imageData.At(x, y).RGBA()
+		row[x] = RGB{int(r >> 8), int(g >> 8), int(b >> 8)}
 	}
 	return row
 }
@@ -60,27 +60,36 @@ func GetTotalDistance(imageData1 image.Image, imageData2 image.Image) float64 {
 
 	if bounds1.Dx() != bounds2.Dx() || bounds1.Dy() != bounds2.Dy() {
 		fmt.Println("Les images n'ont pas la même taille")
-		return 1000000000
+		return 1e9
 	}
-	var matrix [][]RGB
 
+	// Création des matrices RGB
+	matrix1 := make([][]RGB, bounds1.Dy())
+	matrix2 := make([][]RGB, bounds2.Dy())
 	var wg sync.WaitGroup
-	wg.Add(bounds1.MAX.Y)
+
 	for y := bounds1.Min.Y; y < bounds1.Max.Y; y++ {
-		go addRGBRow(imageData1, y, &matrix)
+		wg.Add(1)
+		go func(y int) {
+			defer wg.Done()
+			matrix1[y] = createRGBRow(imageData1, y)
+			matrix2[y] = createRGBRow(imageData2, y)
+		}(y)
 	}
-	wg.Wait() 
-	fmt.Println("Go routines terminés. La matrice remplie")
+	wg.Wait()
+
+	fmt.Println("Matrices remplies. Calcul de la distance totale...")
 
 	totalDistance := 0.0
-	// on fait les distances
+	for i := 0; i < len(matrix1); i++ {
+		totalDistance += rowProcessDistance(matrix1[i], matrix2[i])
+	}
 	return totalDistance
 }
 
 func main() {
 	fmt.Println("Hello, world.")
 
-	// On ignore imageType en utilisant l'underscore
 	imageData1, _, err1 := getImageData("./img/img1.jpg")
 	imageData2, _, err2 := getImageData("./img/img2.jpg")
 
@@ -94,7 +103,9 @@ func main() {
 		}
 		os.Exit(1)
 	}
+	totalDistance := GetTotalDistance(imageData1, imageData2)
 
+	fmt.Println("Total distance:", totalDistance)
 	fmt.Println(imageData1.At(10, 20))
 	fmt.Println(imageData2.At(10, 20))
 }
